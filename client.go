f49a27e7edbbfd1bd3c0b4c4bf2051c67ddab8f0
KilimcininKorOglu <229108989+KilimcininKorOglu@users.noreply.github.com>
2026-04-26 22:19:18 +0300

    chore: increase retry delay to 5s

--- client.go
+++ client.go
@@ -20,7 +20,7 @@ const (
 	userAgent  = "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"
 
 	defaultMaxAttempts = 10
-	retryDelay         = 2 * time.Second
+	retryDelay         = 5 * time.Second
 )
 
 type Config struct {
